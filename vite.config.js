66503d9250685f5328c10ec3dc605aea9a7bf381
creedtech <ayodeleayoolaoke4@gmail.com>
2025-04-15 20:47:15 +0100

    chore: update dependencies and configure Vite plugins
    - Added `vite-plugin-pages` and `vite-plugin-ssr` to dependencies in package.json.
    - Removed unused `toast` import from App.jsx.
    - Commented out Vite plugins configuration for `vite-plugin-prerender`, `vite-plugin-pages`, and `vite-plugin-ssr` in vite.config.js.
    - Set build target to 'esnext' in vite.config.js.

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import prerender from 'vite-plugin-prerender';
+// import ssr from 'vite-plugin-ssr/plugin';
+// import pagesPlugin from 'vite-plugin-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    prerender({
-      staticDir: 'dist',
-      routes: ['/', '/collection', '/product', '/about'],
-    }),
+    // pagesPlugin({
+    //   dirs: 'src/pages',
+    // }),
+    // ssr({
+    //   rootDocument: 'src/app.jsx',
+    //   clientMinify: false,
+    //   serverMinify: false,
+    //   // prerender: true, // Enable prerendering of static pages
+    // }),
   ],
+  build: {
+    target: 'esnext',
+  },
   server: { port: 5173 },
 });
