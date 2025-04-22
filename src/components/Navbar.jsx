f82ed876396f7dde41f0e25ddb5ac613ca831d99
creedtech <ayodeleayoolaoke4@gmail.com>
2025-04-22 23:15:06 +0100

    feat: add high z-index to Sidebar menu for improved visibility on small screens

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -101,7 +101,7 @@ const Navbar = () => {
 
       {/* Sidebar menu for small screens */}
       <div
-        className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${
+        className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all z-[999999] ${
           visible ? 'w-full' : 'w-0'
         }`}
       >
